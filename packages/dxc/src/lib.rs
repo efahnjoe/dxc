59eca14d855ddaaec37f2e37ae5a4fc23d18252e
efahnjoe <efahnjoe@gmail.com>
2025-08-21 00:31:12 +0800

    feat(dxc): introduce dxc-types module and standardize imports
    - Add dxc-types dependency in Cargo.toml
    - Import dxc-types module in lib.rs
    - Organize module import order for consistency

--- packages/dxc/src/lib.rs
+++ packages/dxc/src/lib.rs
@@ -1,9 +1,11 @@
 pub mod prelude {
     pub use dxc_components::*;
-    pub use dxc_themes::*;
     pub use dxc_icons::*;
+    pub use dxc_themes::*;
+    pub use dxc_types::*;
 }
 
 pub use dxc_components::*;
-pub use dxc_themes::*;
 pub use dxc_icons::*;
+pub use dxc_themes::*;
+pub use dxc_types::*;
