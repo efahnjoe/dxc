d945a1cb095fe41da8efcb9df6a388f8b0fcfdd7
efahnjoe <efahnjoe@gmail.com>
2025-08-23 00:48:54 +0800

    feat(dxc-types/direction): add IntoAttributeValue impl for Direction with kebab-case serialization

--- packages/types/src/direction.rs
+++ packages/types/src/direction.rs
@@ -1,7 +1,21 @@
+use dioxus::dioxus_core::AttributeValue;
+use dioxus::prelude::IntoAttributeValue;
 use serde::{Deserialize, Serialize};
+use strum::{Display, EnumString};
 
-#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize)]
+#[derive(Clone, Copy, Debug, PartialEq, Serialize, Deserialize, Display, EnumString)]
+#[strum(serialize_all = "kebab-case")]
 pub enum Direction {
+    #[serde(rename = "vertical")]
+    #[strum(serialize = "vertical")]
     Vertical,
+    #[serde(rename = "horizontal")]
+    #[strum(serialize = "horizontal")]
     Horizontal,
+}
+
+impl IntoAttributeValue for Direction {
+    fn into_value(self) -> AttributeValue {
+        AttributeValue::Text(self.to_string())
+    }
 }
