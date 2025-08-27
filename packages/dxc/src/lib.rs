f68917ad49aced562a4c5157041c597047a34cac
efahnjoe <efahnjoe@gmail.com>
2025-08-27 08:19:21 +0800

    refactor(dxc): update type export paths in prelude
    - Change import from dxc_types::* to dxc_types::components::*

--- packages/dxc/src/lib.rs
+++ packages/dxc/src/lib.rs
@@ -2,7 +2,7 @@ pub mod prelude {
     pub use dxc_components::*;
     pub use dxc_icons::*;
     pub use dxc_themes::*;
-    pub use dxc_types::*;
+    pub use dxc_types::components::*;
 }
 
 pub use dxc_components::*;
